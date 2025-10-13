d5f13ca8f3e988d6ef7a8a62c0efca1c96a9c004
Talha Qazi <talha.nizami01@gmail.com>
2025-10-13 21:27:44 +0500

    image size increase

--- utils/imageUtils.js
+++ utils/imageUtils.js
@@ -165,7 +165,7 @@ const validateImageForFaceRecognition = (imagePath) => {
     const fileSizeInMB = stats.size / (1024 * 1024);
 
     // Check file size (should be between 10KB and 5MB)
-    if (fileSizeInMB < 0.01 || fileSizeInMB > 20) {
+    if (fileSizeInMB < 0.01 || fileSizeInMB > 30) {
       return {
         valid: false,
         message: "Image size should be between 10KB and 5MB",
